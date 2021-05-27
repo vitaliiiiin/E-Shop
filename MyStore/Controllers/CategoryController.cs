90b81129d21bbad1663079b35e90b9f36677025a
Vitalii Nesterenko <nesterenkov90@gmail.com>
2021-05-27 17:46:32 +0300

    Add category controller

---
+++ MyStore/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Data;
+using MyStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
+        }
+    }
+}
