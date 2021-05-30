0bb74590c50973c17ccfd2587d7d40b511ea5371
Vitalii Nesterenko <nesterenkov90@gmail.com>
2021-05-30 13:51:23 +0300

    Add Server validation

--- MyStore/Models/Product.cs
+++ MyStore/Models/Product.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyStore.Models
 {
-    public class Category
+    public class Product
     {
         [Key]
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        [DisplayName("Display Name")]
-        [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "Display Order must be greater than 0")]
-        public int DisplayOrder { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
     }
 }
