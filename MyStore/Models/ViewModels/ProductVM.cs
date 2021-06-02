3c4f49c06db61016a80c3116abc4200f2eeb0cb6
Vitalii Nesterenko <nesterenkov90@gmail.com>
2021-06-02 21:46:08 +0300

    Add contructor

--- MyStore/Models/ViewModels/ProductVM.cs
+++ MyStore/Models/ViewModels/ProductVM.cs
@@ -8,6 +8,10 @@ namespace MyStore.Models.ViewModels
 {
     public class ProductVM
     {
+        public ProductVM()
+        {
+            Product = new Product();
+        }
         public Product Product { get; set; }
         public IEnumerable<SelectListItem> DropDownCategories { get; set; }
         public IEnumerable<SelectListItem> DropDownManufacturers { get; set; }
