2d1ad4ee21031c1f9b8e03323b508d5b0cf40675
Vitalii Nesterenko <nesterenkov90@gmail.com>
2021-06-03 00:51:18 +0300

    Create Cart Controller

---
+++ MyStore/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Data;
+using MyStore.Models;
+using MyStore.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            var shoppingCartList = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                // session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstants.SessionCart);
+            }
+
+            var prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            var prodList = _db.Products.Where(i => prodInCart.Contains(i.Id));
+
+            return View(prodList);
+        }
+    }
+}
