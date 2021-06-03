e1a361da0db2580af56530cb86f194a0c2689654
Vitalii Nesterenko <nesterenkov90@gmail.com>
2021-06-03 15:48:29 +0300

    Add Summary action and view

--- MyStore/Controllers/CartController.cs
+++ MyStore/Controllers/CartController.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyStore.Data;
 using MyStore.Models;
+using MyStore.Models.ViewModels;
 using MyStore.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MyStore.Controllers
@@ -15,6 +17,8 @@ namespace MyStore.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public UserProductVM UserProductVM { get; set; }
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -36,6 +40,40 @@ namespace MyStore.Controllers
             var prodList = _db.Products.Where(i => prodInCart.Contains(i.Id));
 
             return View(prodList);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName(nameof(Index))]
+        public IActionResult IndexPost()
+        {
+
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.Name);
+
+            var shoppingCartList = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                // session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstants.SessionCart);
+            }
+
+            var prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            var prodList = _db.Products.Where(i => prodInCart.Contains(i.Id));
+
+            UserProductVM = new UserProductVM
+            {
+                ApplicationUser = _db.ApplicationUsers.FirstOrDefault(i => i.Id == userId),
+                ProductList = prodList
+            };
+
+            return View(UserProductVM);
         }
 
         public IActionResult RemoveFromCart(int id)
---
+++ MyStore/Models/ViewModels/UserProductVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.Models.ViewModels
+{
+    public class UserProductVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Product> ProductList { get; set; } = new List<Product>();
+    }
+}
