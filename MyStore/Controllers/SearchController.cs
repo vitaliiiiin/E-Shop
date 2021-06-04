839060e5c3f092fcad7c022ed5994efa3da80873
Vitalii Nesterenko <nesterenkov90@gmail.com>
2021-06-04 17:15:25 +0300

    Add Search

---
+++ MyStore/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SearchController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index(string searching)
+        {
+            return View(_db.Products.Where(i => i.Name.Contains(searching) || searching == null)
+                .Include(i => i.Category)
+                .Include(i => i.Manufacturer).ToList());
+        }
+    }
+}
